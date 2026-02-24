2c7f158e92a5a9b03ed24afbae49a95b96c8f054
WEBDEVELOPER-RATTy <chimikey042@gmail.com>
2026-02-24 04:59:53 -0800

    updated honepage

--- index.php
+++ index.php
@@ -685,13 +685,14 @@
                         </div>
                     </div>
                     <div class="container">
-                        <div class="copyright">
+
+                            <!--
                             <div class="row">
                                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
                                     &copy; <a class="border-bottom" href="#">RaTTiz kitchen</a>, All Right
                                     Reserved.
 
-                                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                                    *** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***
                                     Designed By <a class="border-bottom"
                                         href="https://htmlcodex.com">RaTTizScript</a><br><br>
                                     Distributed By <a class="border-bottom" href="https://rattizscript.com"
@@ -709,7 +710,11 @@
                         </div>
                     </div>
                 </div>
+                       
+-->
+            
                 <!-- Footer End -->
+                 
 
 
                 <!-- Back to Top -->
